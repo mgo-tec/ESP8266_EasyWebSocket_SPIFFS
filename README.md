0f064fa94c8acfcbd9b9467523c212b99acfd9e2
mgo-tec <mgo.tec@gmail.com>
2017-04-04 16:46:10 +0900

    Updated Beta ver 1.49
    Added EWS_https_Web_Get class.
    Memory reduced sample sketch.
    Other minor fixes.

---
+++ README.md
@@ -0,0 +1,133 @@
+# ESP8266_SPIFFS_EasyWebSocket BETA ver 1.49
+Simple library for WebSocket communication with ESP-WROOM-02(ESP8266) and smartphone browser.  
+This requires the Arduino core for ESP8266 WiFi chip library.  
+https://github.com/esp8266/arduino  
+This use SPIFFS file system.
+
+My Blog: https://www.mgo-tec.com
+# Change log:
+(1.49) 
+Added a function to GET Web articles by https (SSL).  
+Along with that, include WiFiClientSecure.h.  
+Memory reduced sample sketch.  
+Other minor fixes.  
+  
+-NEW Class-  
+EWS_https_Web_Get  
+(Attention !: Consumes a lot of memory.)  
+  
+(1.48)  
+*Added a color picker (for Android, Windows 10) library that selects the RGB color code for determining colors in the browser.  
+*Added a library that automatically obtains the local IP address and an HTML header division file accompanying it.  
+*Updated the HTML header file and increased the information size of WebSocket communication status display.  
+*Other minor modifications  
+  
+-NEW class-  
+Color_Picker  
+EWS_Dev_AutoLIP_HandShake_str  
+  
+(1.45)  
+A watchdog timer of ESP8266 to create a grace period to operate, has been added here and there yield ().  
+As a result, I think the operation is stable.  
+Sample sketch also has been corrected.  
+
+-NEW class-  
+EWS_BrowserReceiveTextTag2  
+EWS_Status_Text2  
+EWS_WebSocket_Reconnection_Button2  
+EWS_Close_Button2  
+EWS_Window_ReLoad_Button2  
+
+(1.39)  
+*It added support for SoftAP mode.
+
+(1.37)  
+*I the suggestions for improvement from Visyeii's on twitter, I was allowed to adopt because it was very effective.  
+Mr. Visyeii, Thank you very much.
+
+*And speed of loading the spiffs_01.txt file in SPIFFS file system at the time of access from the first of the smartphone browser is much up, display of the browser is now faster.  
+*Fixed a WebSocket reconnect button in (confirmed by ver 9.3.1) iOS.  
+*Connections at the time I was allowed to divert the handleClient function from the library WiFiServer.cpp of Arduino core for ESP8266 WiFi chip in. I think the connection has become more reliable.  
+
+(1.35)  
+*From the Web to GET requests, add a function to extract the text by analyzing the HTML tags.  
+*Add to reconnect button of WebSocket communication only.
+
+(1.3)  
+*Brush up the WebSocket handshake method.  
+*Reconnection is now easier to.  
+*Reload button Add Library.  
+*Momentary button Add Library.  
+*Text box send Add Library  
+(However, up to 120 bytes, including the ID string)  
+*Canvas_Slider was so that data transmission in only touched without slide.  
+*Updated the HTML header file "spiffs_01.txt" be read by SPIFFS file system.
+
+【更新履歴】(Japanese)   
+(1.49)  
+Web記事を https ( SSL )でGETする関数を追加。  
+それに伴い、WiFiClientSecure.h をインクルード。  
+メモリを削減したサンプルスケッチをアップ。  
+その他、軽微な修正。  
+  
+-NEW Class-  
+EWS_https_Web_Get  
+(メモリを多く消費するので注意)  
+  
+(1.48)  
+・ブラウザで色を決定するためのRGBカラーコードを選択するカラーピッカー(Android, Windows10用)ライブラリを追加しました。  
+・ローカルIPアドレスを自動取得するライブラリと、それに伴うHTMLヘッダ分割ファイルを追加しました。  
+・HTMLヘッダファイルを更新し、WebSocket通信Status表示の情報量を増やしました。  
+・その他、軽微な修正  
+  
+-NEW class-  
+Color_Picker  
+EWS_Dev_AutoLIP_HandShake_str  
+  
+(1.45)  
+ESP8266のウォッチドッグタイマを作動させる猶予時間を作るために、所々 yield() を追加しました。  
+それにより、多少動作が安定したと思います。
+サンプルスケッチも修正しました。
+
+-NEW class-  
+EWS_BrowserReceiveTextTag2  
+EWS_Status_Text2  
+EWS_WebSocket_Reconnection_Button2  
+EWS_Close_Button2  
+EWS_Window_ReLoad_Button2  
+
+(1.39)  
+ESP-WROOM-02 (ESP8266) の SoftAPモードに対応しました。詳しくはサンプルスケッチをご覧ください。
+
+(1.37)  
+ツィッター上でVisyeiiさんから改善提案をいただき、とても有効でしたので採用させていただきました。  
+Visyeiiさん、ありがとうございました。
+
+・初回のスマートフォンブラウザからのアクセス時にSPIFFSファイルシステムでspiffs_01.txtファイルを読み込む速度が格段にアップし、ブラウザの表示が早くなりました。  
+・iOS(9.3.1で動作確認済み)でのWebSocket再接続ボタンを修正しました。  
+・コネクション時にArduino core for ESP8266 WiFi chip のライブラリ WiFiServer.cpp から handleClient()関数を流用させていただきました。コネクションがより確実になったかと思います。  
+
+(1.35)  
+・WebからGETリクエストして、HTMLタグを解析してテキストを抽出する関数を追加。  
+・WebSocket通信のみの再接続ボタンを追加。
+
+(1.3)  
+・WebSocketハンドシェイク方法をブラッシュアップ  
+・再接続がしやすくなりました。  
+・再接続(Reload) ボタンライブラリ追加  
+・瞬時ボタン（モーメンタリーボタン）ライブラリ追加  
+・テキストボックス送信ライブラリ追加  
+（※ただし、ID文字列も含め120バイトまで）  
+・Canvas_Slider をスライドしなくてもタッチしただけでデータ送信するようにしました。  
+・SPIFFSファイルシステムで読み出すHTMLヘッダファイル spiffs_01.txt を更新しました。  
+# Credits and license
+*Licensed under the GNU LESSER GENERAL PUBLIC LICENSE Version 2.1
+
+*Copyright (c) 2016 mgo-tec
+
+Other usage is Japanese, please visit my blog.
+
+My Blog:  https://www.mgo-tec.com  
+(ver1.39) https://www.mgo-tec.com/blog-entry-softap-esp8266-easywebsocket139.html  
+(ver1.35) https://www.mgo-tec.com/blog-entry-wroom-websocket-messageboard-05.html  
+(ver1.3) https://www.mgo-tec.com/blog-entry-easywebsocket-beta13.html
---
+++ src/EasyWebSocket.h
@@ -0,0 +1,109 @@
+/*
+  EasyWebSocket.h - WebSocket for ESP-WROOM-02 ( esp8266 - SPIFFS use)
+  Beta version 1.49
+
+Copyright (c) 2016 Mgo-tec
+This library improvement collaborator is Mr.Visyeii.
+
+This library is used by the Arduino IDE(Tested in ver1.6.12).
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+Reference LGPL-2.1 license statement --> https://opensource.org/licenses/LGPL-2.1
+
+Reference Blog --> https://www.mgo-tec.com
+
+The esp8266 core for Arduino(Tested in ver2.3.0) --> https://github.com/esp8266/Arduino
+Released under the GNU LESSER GENERAL PUBLIC LICENSE Version 2.1
+
+ESP8266WiFi.h - Included WiFi library for esp8266
+WiFiServer.cpp - The library modification
+Copyright (c) 2014 Ivan Grokhotkov.
+This file is part of the esp8266 core for Arduino environment.
+Released under the GNU LESSER GENERAL PUBLIC LICENSE Version 2.1
+
+WiFiClientSecure.cpp - Variant of WiFiClient with TLS support
+Copyright (c) 2015 Ivan Grokhotkov.
+This file is part of the esp8266 core for Arduino environment.
+Released under the GNU LESSER GENERAL PUBLIC LICENSE Version 2.1
+
+Hash.h - Included library
+Copyright (c) 2015 Markus Sattler.
+This file is part of the esp8266 core for Arduino environment.
+Released under the GNU LESSER GENERAL PUBLIC LICENSE Version 2.1
+
+FS.h(SPIFFS-File system) - Included library
+-->https://github.com/esp8266/arduino-esp8266fs-plugin
+Copyright (c) 2015 Ivan Grokhotkov. All rights reserved.
+Released under the GNU LESSER GENERAL PUBLIC LICENSE Version 2.1
+*/
+
+#ifndef EasyWebSocket_h_
+#define EasyWebSocket_h_
+#include "Arduino.h"
+#include "ESP8266WiFi.h"
+#include "WiFiClientSecure.h"
+//#include "ESP8266WebServer.h"
+#include "Hash.h"
+#include "FS.h"
+
+enum HTTPClientStatus1 { HC_NONE1, HC_WAIT_READ1, HC_WAIT_CLOSE1 };
+#define HTTP_MAX_DATA_WAIT 1000 //ms to wait for the client to send the request
+#define HTTP_MAX_CLOSE_WAIT 2000 //ms to wait for the client to close the connection
+
+class EasyWebSocket
+{
+public:
+  EasyWebSocket();
+
+  void AP_Connect(const char* ssid, const char* password);
+  void SoftAP_setup(const char* ssid, const char* password);
+  void handleClient();
+  void EWS_HandShake(String res_html1, String res_html2, String res_html3, String res_html4, String res_html5, String res_html6, String res_html7);
+  void EWS_Dev_AutoLIP_HandShake_str(const char* HTML_head_file1, IPAddress res_LIP, const char* HTML_head_file2, String res_html1, String res_html2, String res_html3, String res_html4, String res_html5, String res_html6, String res_html7);
+  void EWS_HTTP_Responce();
+  void Hash_Key(String h_req_key, char* h_resp_key);
+  void EWS_ESP8266_Str_SEND(String str, String id);
+  void EWS_PING_SEND();
+  String EWS_ESP8266CharReceive(uint16_t pTime);
+  String EWS_ESP8266_Binary_Receive();
+  String EWS_OnOff_Button(String button_id, uint16_t width, uint16_t height, uint8_t font_size, String f_color, String b_color);
+  String EWS_On_Momentary_Button(String button_id, String text, uint16_t width, uint16_t height, uint8_t font_size, String f_color, String b_color);
+  String EWS_Touch_Slider_BT(String slider_id, String vbox_id);
+  String EWS_Touch_Slider_T(String slider_id, String txt_id);
+  String EWS_Mouse_Slider_BT(String slider_id, String vbox_id);
+  String EWS_Mouse_Slider_T(String slider_id, String txt_id);
+  String EWS_Sl_BoxText(String vbox_id, uint16_t width, uint16_t height, uint8_t font_size, String color);
+  String EWS_Sl_Text(String text_id, uint8_t font_size, String color);
+  String EWS_Body_style(String text_color, String bg_color);
+  String EWS_BrowserReceiveTextTag(String id, uint8_t font_size, String fnt_col);
+	String EWS_BrowserReceiveTextTag2(String id, String name, String b_color, uint8_t font_size, String fnt_col);
+  String EWS_Close_Button(String name, uint16_t width, uint16_t height, uint8_t font_size);
+	String EWS_Close_Button2(String name, String BG_col, uint16_t width, uint16_t height, String font_col, uint8_t font_size);
+  String EWS_Window_ReLoad_Button(String name, uint16_t width, uint16_t height, uint8_t font_size);
+	String EWS_Window_ReLoad_Button2(String name, String BG_col, uint16_t width, uint16_t height, String font_col, uint8_t font_size);
+  String EWS_WebSocket_Reconnection_Button(String name, uint16_t width, uint16_t height, uint8_t font_size);
+  String EWS_WebSocket_Reconnection_Button2(String name, String BG_col, uint16_t width, uint16_t height, String font_col, uint8_t font_size);
+	String EWS_BrowserSendRate();
+  String EWS_Status_Text(uint8_t font_size, String color);
+  String EWS_Status_Text2(String name, String b_color, uint8_t font_size, String f_color);
+  String EWS_Canvas_Slider_T(String slider_id, uint16_t width, uint16_t height, String frame_col, String fill_col);
+  String EWS_TextBox_Send(String id, String txt, String BT_txt);
+  String EWS_Web_Get(const char* host, String target_ip, char char_tag, String Final_tag, String Begin_tag, String End_tag, String Paragraph);
+  String EWS_https_Web_Get(const char* host, String target_ip, char char_tag, String Final_tag, String Begin_tag, String End_tag, String Paragraph);
+	void Favicon_Response(String str, uint8_t ws, uint8_t ini_htm, uint8_t up_f);
+  String Color_Picker(uint16_t top_px, uint16_t left_px, String default_col, String ID);
+
+	
+private:
+  boolean _Ini_html_on = false;
+  boolean _WS_on = false;
+  boolean _Upgrade_first_on;
+  char _Android_or_iPad;
+  uint32_t _PingLastTime = 0;
+  uint32_t _PongLastTime = 0;
+};
+
+#endif
